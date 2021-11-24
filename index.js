6f3377a9fd4da7fab3ab5d0e0730f39106c3cac2
ram99-sys <raghuram0024@gmail.com>
2021-11-24 12:35:54 +0000

    react js coding-practice1

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = (
+  //   Write your code here.
+  <div className="bg-container">
+    <div class="align-heading">
+      <h1 className="heading-element">Super Over League</h1>
+    </div>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="image-size"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="image-size"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
